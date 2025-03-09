b4bbd01d96a9d346cf3075c0fbf8a84e422132f7
messirodrigues708 <messirodrigues708@gmail.com>
2025-03-09 07:42:35 +0000

    Add 4.rs

---
+++ src/4.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut x = 1;
+    let y = 2;
+    if x < y {
+        println!("{} is less than {}", x, y);
+    } else {
+        println!("{} is greater than or equal to {}", x, y);
+    }
+}
